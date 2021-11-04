4df8927d9e09fccd3dcd99d1a8013fb559695a1e
Sharon Thomeh <sharon.thomeh@gmail.com>
2021-11-04 14:57:47 -0500

    Updated Client.java client setup

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,6 +6,47 @@ import java.net.Socket;
 import java.util.function.Consumer;
 
 public class Client extends Thread {
+	Socket socketClient;
+	
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	
+	private Consumer<Serializable> callback;
+	
+	Client(Consumer<Serializable> call){
+	
+		callback = call;
+	}
+	
+	public void run() {
+		
+		try {
+		socketClient= new Socket("127.0.0.1",5555);
+	    out = new ObjectOutputStream(socketClient.getOutputStream());
+	    in = new ObjectInputStream(socketClient.getInputStream());
+	    socketClient.setTcpNoDelay(true);
+		}
+		catch(Exception e) {}
+		
+		while(true) {
+			 
+			try {
+			String message = in.readObject().toString();
+			callback.accept(message);
+			}
+			catch(Exception e) {}
+		}
+	
+    }
+	
+	public void send(String data) {
+		
+		try {
+			out.writeObject(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	
 }
