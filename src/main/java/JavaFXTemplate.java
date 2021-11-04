2b69326190cd8341ce09c26d4a7fac2b2e42e762
Sharon Thomeh <sharon.thomeh@gmail.com>
2021-11-04 14:58:44 -0500

    Update JavaFXTemplate.java to create GUI

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -1,29 +1,105 @@
+import java.util.HashMap;
+
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
-
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.util.Duration;
 
 public class JavaFXTemplate extends Application {
+	
+	
+	private TextField portNumber, ipAddress, biddingAmount;
+	private Button connectToServer, exit, StartGame, StartNewGame, ExitGame;
+    private Text resultsPmpt, playerPmpt, bankerPmpt, biddingAmountPmpt, 
+    biddingTypePmpt, portNumberPmpt, ipAddressPmpt;
+	private HashMap<String, Scene> sceneMap;
+	private HashMap<Integer, String> cardImages;
+	private HBox VBoxPortAndIP, VBoxConnectAndExit, biddingAmountAndRadio, ResultsAndButtons;
+	private VBox textAndBut, startAndExit, vBoxForBiddingScene, startNewGameAndExit;
+	private Client clientConnection;
+	private RadioButton player, banker, draw;
+	private ToggleGroup radioButtons;
+	ListView<String> results;
+	PauseTransition pauseForWinner = new PauseTransition(Duration.seconds(3));
+	PauseTransition pauseForCardDisplay = new PauseTransition(Duration.seconds(3));
+	private EventHandler<ActionEvent> startnewGameHandler, exitGameHandler,
+	leaveServerhandler, startGamehandler, radioButtonHandler, 
+	connectToServerHandler;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
 	}
 
-	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
+	
+		primaryStage.setTitle("Baccarat Client");
+		
+		this.connectToServer = new Button("Connect To Server");
+		
+		this.connectToServer.setOnAction(e-> {primaryStage.setScene(sceneMap.get("biddingScene"));
+											primaryStage.setTitle("This is a client");
+											clientConnection = new Client(data->{
+							Platform.runLater(()->{results.getItems().add(data.toString());
+											});
+							});
+							
+											clientConnection.start();
+		});
+		
+		
+		sceneMap = new HashMap<String, Scene>();
+		sceneMap.put("portAndIDScene",  createPortIDScene());
+		sceneMap.put("gameScene",  createGameScene());
+		sceneMap.put("biddingScene",  createBiddingScene());
+		
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+		
+	}
+	
+	
+	public Scene createPortIDScene() {
 		
 		
 		
-				
-		Scene scene = new Scene(new VBox(), 700,700);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+	}
+	
+	public Scene createGameScene() {
+		
+		
+		
+	}
+	
+	public Scene createBiddingScene() {
+		
+		
+		
+	}
+	
+	public void makeCards() {
+		
 	}
 
 }
