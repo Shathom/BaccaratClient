c9bf70a47b0badedcac89394dd591813e1149cce
Sharon Thomeh <sharon.thomeh@gmail.com>
2021-11-06 20:19:19 -0500

    Update JavaFXTemplate.java
    set up server scenes

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -26,7 +26,7 @@ public class JavaFXTemplate extends Application {
 	
 	private TextField portNumber, ipAddress, biddingAmount;
 	private Button connectToServer, exit, StartGame, StartNewGame, ExitGame;
-    private Text resultsPmpt, playerPmpt, bankerPmpt, biddingAmountPmpt, 
+    private Text resultsPmpt, currentWinningsPmpt, playerPmpt, bankerPmpt, biddingAmountPmpt, 
     biddingTypePmpt, portNumberPmpt, ipAddressPmpt;
 	private HashMap<String, Scene> sceneMap;
 	private HashMap<Integer, String> cardImages;
@@ -36,6 +36,7 @@ public class JavaFXTemplate extends Application {
 	private RadioButton player, banker, draw;
 	private ToggleGroup radioButtons;
 	ListView<String> results;
+	ListView<String> currentWinnings;
 	PauseTransition pauseForWinner = new PauseTransition(Duration.seconds(3));
 	PauseTransition pauseForCardDisplay = new PauseTransition(Duration.seconds(3));
 	private EventHandler<ActionEvent> startnewGameHandler, exitGameHandler,
@@ -52,6 +53,9 @@ public class JavaFXTemplate extends Application {
 		primaryStage.setTitle("Baccarat Client");
 		
 		this.connectToServer = new Button("Connect To Server");
+		
+		// when player presses a button, you would send info from textfields and create an instance
+		// of bainfo class and pass innto the send funciton
 		
 		this.connectToServer.setOnAction(e-> {primaryStage.setScene(sceneMap.get("biddingScene"));
 											primaryStage.setTitle("This is a client");
